4a6e0ba2429454529a362914b50ebe091bf16705
youssefshawwa1 <youssefshawwa1@gmail.com>
2025-09-15 07:43:11 -0400

    my solution

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Footer = () =>{
+
+
+
+    return(
+        <>
+        <footer>
+            <p>&copy; Event Planner Organization. All rights reserved.</p>
+        </footer>
+        </>
+    );
+}
+export default Footer;
